58f97ad2dd569d9350504212456bd7be384292e2
iTech <iTech@DESKTOP-RT7KEOQ>
2026-08-15 01:20:56 +0300

    Question01

--- OOP.Assignment03/Program.cs
+++ OOP.Assignment03/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Question01
+
+            /// 01) What is the difference between Method Overloading and Method Overriding?
+            ///
+            /// 
+            /// Method Overloading means having multiple methods with the same name
+            /// but different parameters.
+            ///
+            /// Method Overriding means that a child class provides a new implementation
+            /// for a virtual method inherited from the parent class.
+            ///
+            /// Example:
+            ///
+            /// public void UpdateWeight(double newWeight)
+            /// {
+            /// }
+            ///
+            /// public void UpdateWeight(double newWeight, double extraPackingWeight)
+            /// {
+            /// }
+            ///
+            /// // The two methods have the same name but different parameters.
+            /// // This is Method Overloading.
+
+            #endregion
+
+
+
+
+
+
         }
     }
 }
