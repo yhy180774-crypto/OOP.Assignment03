0563d85aa8339f19e320ab39417f1e2a70530d60
iTech <iTech@DESKTOP-RT7KEOQ>
2026-08-15 01:13:56 +0300

    Add project files.

---
+++ OOP.Assignment03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP.Assignment03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
